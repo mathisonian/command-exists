609af63d32c7b408ec26db5cec5925d1f213bdda
Matthew Conlen <mc@mathisonian.com>
2014-09-21 16:40:56 -0400

    initial commit

---
+++ test/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var expect = require('expect.js');
+var commandExists = require('..');
+
+
+describe('commandExists()', function(){
+
+    it('it should find a command named ls', function(done){
+        commandExists('ls', function(err, exists) {
+            expect(err).to.be(null);
+            expect(exists).to.be(true);
+            done();
+        });
+    });
+
+    it('it should not find a command named fdsafdsafdsafdsafdsa', function(done){
+        commandExists('fdsafdsafdsafdsafdsa', function(err, exists) {
+            expect(err).to.be(null);
+            expect(exists).to.be(false);
+            done();
+        });
+    });
+
+});
