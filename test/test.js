7be9e1a13e5f75b2d9100a6f3bbb80257998c495
Arthur Silber <arthur@arthursilber.de>
2016-07-26 11:52:49 +0200

    Extends commandExists to use where on windows

--- test/test.js
+++ test/test.js
@@ -2,11 +2,17 @@
 
 var expect = require('expect.js');
 var commandExists = require('..');
+var isUsingWindows = process.platform == 'win32'
 
 describe('commandExists()', function(){
 
-    it('it should find a command named ls', function(done){
-        commandExists('ls', function(err, exists) {
+    it('it should find a command named ls or dir', function(done){
+        var commandToUse = 'ls'
+        if (isUsingWindows) {
+            commandToUse = 'dir'
+        }
+
+        commandExists(commandToUse, function(err, exists) {
             expect(err).to.be(null);
             expect(exists).to.be(true);
             done();
